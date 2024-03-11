0fa3146a60e5618a69df23eb25ab86d797103355
Путинцев Никита <nputintsev@neoflex.ru>
2024-03-11 23:13:26 +0300

    feat: init commit

--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -25,7 +25,6 @@ export const Circle: React.FC = () => {
         } else if (rotateDiff < -180) {
             rotateDiff += 360;
         }
-
         setRotateValue(currentPosition + rotateDiff);
     }
     return (
